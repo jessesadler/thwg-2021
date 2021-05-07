1946bc536a9ac2a2857e449ca1872ad061d0accd
Jesse Sadler <jsadler@ucla.edu>
2021-05-06 20:28:13 -0400

    Create time series data
    * Script to create time series, fill data, and fill open accounts to end book.
    * Write running account data

--- scripts/time-series-data.R
+++ scripts/time-series-data.R
@@ -1,5 +1,8 @@
 ## Create time series data ##
 
+# Create time series data, fill in missing dates,
+# and have all open accounts go to the end of the book
+
 library(readr)
 library(dplyr)
 library(tidyr)
@@ -8,8 +11,12 @@ library(debkeepr)
 
 transactions <- read_rds("data/transactions.rds")
 
+
+# Create running accounts and fill data -----------------------------------
+
 # Cast to deb_decimal
 # tidyr::fill() does not work with deb_lsd
+# And it speeds things up
 transactions_dec <- transactions %>%
   arrange(date) %>%
   mutate(lsd = deb_as_decimal(lsd))
@@ -30,52 +37,95 @@ running <- bind_rows(credit, debit) %>%
   # Cumulative
   mutate(current = cumsum(day))
 
-# Fill in missing days
+
+# Fill in missing days to last day with a transaction for each account
 running_fill <- running %>% # running has to be grouped by account_id
   tidyr::complete(date = seq(min(date), max(date), by = "1 day"),
                   # Days with no transactions = 0
                   fill = list(day = 0)) %>%
   # Current stays the same until next day with transaction
-  tidyr::fill(current) %>%
+  tidyr::fill(current)
+
+
+# Fill in values to last day of book for open accounts --------------------
+
+# Distinguish between closed accounts
+closed_ids <- running_fill %>%
+  filter(date == max(date) & near(current, 0)) %>%
+  pull(account_id)
+
+open_ids <- running_fill %>%
+  filter(date == max(date) & !near(current, 0)) %>%
+  pull(account_id)
+
+# Check that lengths are correct
+length(c(closed_ids, open_ids)) == length(unique(running$account_id))
+
+# Create separate closed and open tibbles
+closed_running <- running_fill %>%
+  filter(account_id %in% closed_ids)
+
+open_running <- running_fill %>%
+  filter(account_id %in% open_ids)
+
+# Check that lengths are correct
+nrow(closed_running) + nrow(open_running) == nrow(running_fill)
+
+# Fill dates of open account to end of account book
+open_full_running <- open_running %>%
+  tidyr::complete(date = seq(min(date), max(transactions$date), by = "1 day"),
+                  # Days with no transactions = 0
+                  fill = list(day = 0)) %>%
+  # Current stays the same until next day with transaction
+  tidyr::fill(current)
+
+# Check work
+
+# deb_open has same values as open_full_running on last day
+open <- deb_open(transactions)
+
+y <- open_full_running %>%
+  filter(date == max(date)) %>%
+  mutate(current = deb_as_lsd(current)) %>%
+  pull(current)
+
+all.equal(open$lsd, y)
+
+# Last date for all open accounts is max(date) of transactions
+open_full_running %>%
+  filter(date == max(date)) %>%
+  ungroup() %>%
+  distinct(date) %>%
+  pull()
+
+
+# Bring things together ---------------------------------------------------
+
+complete_running_fill <- bind_rows(closed_running, open_full_running) %>%
+  # Take out observations in gap between 15 March 1585 and 5 September 1593
+  filter(date <= ymd("1585-03-16") | date >= ymd("1594-09-04")) %>%
+  arrange(date, account_id) %>%
   # Cast back to deb_lsd
   mutate(day = deb_as_lsd(day),
          current = deb_as_lsd(current)) %>%
   # Now ungroup
-  ungroup() %>%
-  # Take out observations in gap between 15 March 1585 and 5 September 1593
-  filter(date <= ymd("1585-03-16") | date >= ymd("1594-09-04"))
+  ungroup()
 
+# Check work: Amount of observations added to open accounts
+nrow(open_full_running) - nrow(open_running)
+nrow(bind_rows(closed_running, open_full_running)) - nrow(running_fill)
 
-# Check work:
-
-open <- deb_open(transactions)
-last_day <- running_fill %>%
+last_day <- complete_running_fill %>%
   # Need to group_by() account since most accounts do not go to last day of books
   group_by(account_id) %>%
   filter(date == max(date) & current != 0)
 
 all.equal(open$lsd, last_day$current)
 
-# Dealing with gap of dates
-dfl12_002 <- running_fill %>%
-  filter(account_id == "dfl12_002")
+# Sum of daily transactions should be £0
+# Credits - debits
+sum(complete_running_fill$day)
 
-ymd("1585-03-16") - ymd("1582-11-08") + ymd("1594-12-20") - ymd("1594-09-04")
+# Write data --------------------------------------------------------------
 
-
-gap <- dfl12_002 %>%
-  filter(date <= lubridate::ymd("1585-03-16") | date >= lubridate::ymd("1594-09-04"))
-
-library(ggplot2)
-
-ggplot(dfl12_002) +
-  geom_line(aes(x = date, y = as.numeric(current), group = account_id))
-
-ggplot(dfl12_002) +
-  geom_line(aes(x = date, y = as.numeric(current), group = account_id))
-
-ggplot(dfl12_002) +
-  geom_point(aes(x = date, y = as.numeric(current)))
-
-ggplot(gap) +
-  geom_point(aes(x = date, y = as.numeric(current)))
+write_rds(complete_running_fill, "data/running_accounts.rds")
